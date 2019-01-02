801dc3f1504b009bbc45fcb4836e7de2c47552f3
John Wen <John.Wen@ehealth.com>
2019-01-02 13:38:15 +0800

    add str and re and difflib

---
+++ bulit-in-libraries/text/_re.py
@@ -0,0 +1,5 @@
+# coding: utf-8 
+import re 
+
+# 首先是正则表达式的规则
+# 其次是python正则表达式的方法
---
+++ bulit-in-libraries/text/_str.py
@@ -0,0 +1,14 @@
+s = "hello world"
+print(s.capitalize())
+print(s.casefold()) 
+print(s.count('h')) # 1
+print(s.find('llo')) #2
+print(s.index('lo')) # 3
+print(s.join(['john','wen'])) # johnhello worldwen
+print(s.replace('lo','rl'))
+print(s.splitlines())
+print(s.title()) # 首字母大写
+print(s.zfill(10))
+print(s.strip()) 
+print(s.lower())
+print(s.upper())
